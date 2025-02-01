dd0c12fa3a47e940faf8afdbcc4282cd60921030
francescorichard <francesco.richard.02@gmail.com>
2025-02-01 12:42:44 +0100

    Update week00_prep_answers.py

--- week00_prep/week00_prep_answers.py
+++ week00_prep/week00_prep_answers.py
@@ -1,1 +1,11 @@
+# Step 1: Define our function
+def square(x):  # inputs to the function are placed inside the parentheses
+    """Calculate the square of a function."""  # This line is called a "docstring"; it is optional but recommended! It lets you explain what the function does.
+    return x**2  # define what the function returns. Note that ** is the power operator, * is multiply.
 
+# Step 2: Define our inputs and then call the function
+x = 2  # define a variable "x", which has a value of 2
+y = square(x)  # call function "square" on variable x, and assign the output of "square" to variable "y"
+print('The value of y is', y)  # print the value of variable "y" to the console
+
+x = 16
